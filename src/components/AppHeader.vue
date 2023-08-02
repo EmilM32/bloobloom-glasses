3ff8f4337eac3cb5c54b5d7fb873ae320c5e6aae
Emil <emajer32@gmail.com>
2023-08-02 14:18:26 +0200

    add sub header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,4 +5,12 @@
   <div class="h-14 border-black border-b flex items-center justify-between">
     <div class="ml-10 hover:underline cursor-pointer">MENU</div>
   </div>
+  <div class="grid md:grid-cols-3 grid-cols-1 border-black border-b">
+    <div class="hidden md:block"></div>
+    <div class="text-3xl font-bold flex justify-center items-center border-black md:border-r md:border-l border-b md:border-b-0">Title</div>
+    <div class="flex flex-row">
+      <div class="border-black border-r md:w-1/4 w-1/2 flex justify-center items-center cursor-pointer">COLOUR</div>
+      <div class="border-black md:border-r md:w-1/4 w-1/2 flex justify-center items-center cursor-pointer">SHAPE</div>
+    </div>
+  </div>
 </template>
