7541730dc247318de9ec1fb62ffaeb0c3a3c0d75
Emil <emajer32@gmail.com>
2023-08-02 13:43:27 +0200

    add app header component

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="h-14 border-black border-b flex items-center justify-between">
+    <div class="ml-10 hover:underline cursor-pointer">MENU</div>
+  </div>
+</template>
