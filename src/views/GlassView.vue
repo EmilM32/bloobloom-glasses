e2942237a67604ed097c93f33cafc22b04c62ef6
Emil <emajer32@gmail.com>
2023-08-03 09:25:55 +0200

    update rwd

--- src/views/GlassView.vue
+++ src/views/GlassView.vue
@@ -27,7 +27,7 @@ const { isFetching, data, error } = useFetch<Glasses>(url).json();
 </script>
 
 <template>
-  <div class="grid grid-cols-1 md:grid-cols-3 gap-[1px] bg-black border-black border-b">
+  <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-[1px] bg-black border-black border-b">
     <SingleGlassItem v-for="item in data?.glasses" :key="`glass-item-${item.id}`" :title="item.name"
       :variants="item.glass_variants" class="bg-white" />
   </div>
