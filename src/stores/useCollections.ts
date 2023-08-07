0b0e480273bb8906de48a88d45ce49053e22a62f
Emil <emajer32@gmail.com>
2023-08-07 10:27:49 +0200

    add infinite scroll

--- src/stores/useCollections.ts
+++ src/stores/useCollections.ts
@@ -5,12 +5,18 @@ import { defineStore } from 'pinia';
 interface CollectionsState {
   _collections: Collection[];
   _glasses: Glass[];
+  _page: number;
+  _isFetching: boolean;
+  _collectionTotalCount: number;
 }
 
 export const useCollectionsStore = defineStore('collections', {
   state: (): CollectionsState => ({
     _collections: [],
-    _glasses: []
+    _glasses: [],
+    _page: 1,
+    _isFetching: false,
+    _collectionTotalCount: 0,
   }),
   getters: {
     collections: (state) => {
@@ -19,6 +25,12 @@ export const useCollectionsStore = defineStore('collections', {
     glasses: (state) => {
       return state._glasses;
     },
+    page: (state) => {
+      return state._page;
+    },
+    isFetching: (state) => {
+      return state._isFetching;
+    },
   },
   actions: {
     setCollections(collections: Collection[]) {
@@ -26,13 +38,42 @@ export const useCollectionsStore = defineStore('collections', {
     },
     async fetchGlasses(url: string) {
       try {
+        this._isFetching = true;
         const { data } = await useFetch<Glasses>(url).json();
         this._glasses = data.value.glasses;
+        this._collectionTotalCount = data.value.meta.total_count;
       } catch (error) {
         let message = 'Something went wrong';
         if (error instanceof Error) message = error.message;
         throw new Error(message);
+      } finally {
+        this._isFetching = false;
       }
+    },
+    async fetchMoreGlasses(url: string) {
+      try {
+        if (this._glasses.length === this._collectionTotalCount) return;
+
+        this._isFetching = true;
+        const { data } = await useFetch<Glasses>(url).json();
+        this._glasses.push(...data.value.glasses);
+        this._collectionTotalCount = data.value.meta.total_count;
+      } catch (error) {
+        let message = 'Something went wrong';
+        if (error instanceof Error) message = error.message;
+        throw new Error(message);
+      } finally {
+        this._isFetching = false;
+      }
+    },
+    incrementPage() {
+      this._page++;
+    },
+    resetPage() {
+      this._page = 1;
+    },
+    resetTotalCount() {
+      this._collectionTotalCount = 0;
     },
   },
 })
--- src/views/GlassView.vue
+++ src/views/GlassView.vue
@@ -2,34 +2,64 @@
 import { useCollectionsStore } from '@/stores/useCollections';
 import { buildCollectionUrl } from '@/utils/collection-url';
 import SingleGlassItem from '@/components/SingleGlassItem.vue';
+import AppLoading from '@/components/AppLoading.vue';
 import { useFiltersStore } from '@/stores/useFilters';
 import { ITEMS_PER_PAGE } from '@/constans';
-import { watch } from 'vue';
+import { computed, onMounted, onUnmounted, ref, watch } from 'vue';
 
 const collectionStore = useCollectionsStore();
 const collections = collectionStore.collections;
 
 const filtersStore = useFiltersStore();
 
-watch(filtersStore, () => {
-  const url = buildCollectionUrl(filtersStore.category, {
+const getCollectionUrl = () => {
+  return buildCollectionUrl(filtersStore.category, {
     page: {
       limit: ITEMS_PER_PAGE,
-      number: 1,
+      number: collectionStore.page,
     },
     filters: {
       colour: filtersStore.colours,
       shape: filtersStore.shapes,
     }
   });
+};
 
-  collectionStore.fetchGlasses(url);
+watch(filtersStore, () => {
+  collectionStore.resetPage();
+  collectionStore.resetTotalCount();
+  collectionStore.fetchGlasses(getCollectionUrl());
 });
+
+const scrollComponent = ref<HTMLElement | null>(null);
+
+onMounted(() => {
+  window.addEventListener("scroll", handleScroll)
+})
+
+onUnmounted(() => {
+  window.removeEventListener("scroll", handleScroll)
+})
+
+const isLoading = computed<boolean>(() => {
+  return collectionStore.isFetching;
+})
+
+const handleScroll = () => {
+  let element = scrollComponent.value!
+  if (element.getBoundingClientRect().bottom <= (window.innerHeight + 150) && !isLoading.value) {
+    collectionStore.incrementPage();
+    collectionStore.fetchMoreGlasses(getCollectionUrl());
+  }
+}
 </script>
 
 <template>
-  <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-[1px] bg-black border-black border-b">
-    <SingleGlassItem v-for="item in collectionStore.glasses" :key="`glass-item-${item.id}`" :title="item.name"
-      :variants="item.glass_variants" class="bg-white" />
+  <div ref="scrollComponent">
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-[1px] bg-black border-black border-b">
+      <SingleGlassItem v-for="item in collectionStore.glasses" :key="`glass-item-${item.id}`" :title="item.name"
+        :variants="item.glass_variants" class="bg-white" />
+    </div>
+    <AppLoading v-if="isLoading" />
   </div>
 </template>
